1577b6f94a87b27e18d792cb439c39aaa17e45d5
natanai <109430106+natanai@users.noreply.github.com>
2026-07-31 18:43:31 -0500

    Add guide-linked Johto evolutions

--- reference-data.js
+++ reference-data.js
@@ -3,6 +3,9 @@ const REFERENCE_POKEMON=[
   {d:173,e:"Cleffa",f:"Mélo",j:"ピィ",r:"Pī",t:["Normal"],v:[0,0,0,0,1,0]},
   {d:174,e:"Igglybuff",f:"Toudoudou",j:"ププリン",r:"Pupurin",t:["Normal"],v:[1,0,0,0,0,0]},
   {d:182,e:"Bellossom",f:"Joliflor",j:"キレイハナ",r:"Kireihana",t:["Grass"],v:[0,0,0,0,3,0]},
+  {d:196,e:"Espeon",f:"Mentali",j:"エーフィ",r:"Ēfi",t:["Psychic"],v:[0,0,0,2,0,0]},
+  {d:197,e:"Umbreon",f:"Noctali",j:"ブラッキー",r:"Burakkī",t:["Dark"],v:[0,0,0,0,2,0]},
+  {d:208,e:"Steelix",f:"Steelix",j:"ハガネール",r:"Haganēru",t:["Steel","Ground"],v:[0,0,2,0,0,0]},
   {d:243,e:"Raikou",f:"Raikou",j:"ライコウ",r:"Raikō",t:["Electric"],v:[0,0,0,1,0,2]},
   {d:244,e:"Entei",f:"Entei",j:"エンテイ",r:"Entei",t:["Fire"],v:[1,2,0,0,0,0]},
   {d:245,e:"Suicune",f:"Suicune",j:"スイクン",r:"Suikun",t:["Water"],v:[0,0,1,0,2,0]}
