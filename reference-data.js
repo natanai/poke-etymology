6a2e58d24f8a60434dec962ed901f71392cb49a5
natanai <109430106+natanai@users.noreply.github.com>
2026-08-08 16:58:45 -0500

    Add Blissey guide reference

--- reference-data.js
+++ reference-data.js
@@ -6,6 +6,7 @@ const REFERENCE_POKEMON=[
   {d:212,e:"Scizor",f:"Cizayox",j:"ハッサム",r:"Hassamu",t:["Bug","Steel"],v:[0,2,0,0,0,0]},
   {d:230,e:"Kingdra",f:"Hyporoi",j:"キングドラ",r:"Kingudora",t:["Water","Dragon"],v:[0,1,0,1,1,0]},
   {d:233,e:"Porygon2",f:"Porygon2",j:"ポリゴン２",r:"Porigon Tsū",t:["Normal"],v:[0,0,0,2,0,0]},
+  {d:242,e:"Blissey",f:"Leuphorie",j:"ハピナス",r:"Hapinasu",t:["Normal"],v:[3,0,0,0,0,0]},
   {d:243,e:"Raikou",f:"Raikou",j:"ライコウ",r:"Raikō",t:["Electric"],v:[0,0,0,1,0,2]},
   {d:244,e:"Entei",f:"Entei",j:"エンテイ",r:"Entei",t:["Fire"],v:[1,2,0,0,0,0]},
   {d:245,e:"Suicune",f:"Suicune",j:"スイクン",r:"Suikun",t:["Water"],v:[0,0,1,0,2,0]}
